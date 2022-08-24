cd5f217db5883b2b937654019bdde1c329f4b6fd
José Robson <joserobsonsiqueira23@hotmail.com>
2022-08-23 22:58:14 -0300

    feat: eureka server created

--- payrollapi/src/main/java/br/com/rbn/payrollapi/PayrollapiApplication.java
+++ payrollapi/src/main/java/br/com/rbn/payrollapi/PayrollapiApplication.java
@@ -2,10 +2,12 @@ package br.com.rbn.payrollapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class PayrollapiApplication {
 
 	public static void main(String[] args) {
--- payrollapi/src/main/java/br/com/rbn/payrollapi/resources/PayrollResource.java
+++ payrollapi/src/main/java/br/com/rbn/payrollapi/resources/PayrollResource.java
@@ -3,19 +3,23 @@ package br.com.rbn.payrollapi.resources;
 import br.com.rbn.payrollapi.domain.Payroll;
 import br.com.rbn.payrollapi.service.PayrollService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/api/payments")
 public class PayrollResource {
 
+    private final Environment env;
     private final PayrollService service;
 
     @GetMapping(value = "/{workerId}")
     public ResponseEntity<Payroll> getPayment(@PathVariable Long workerId, @RequestBody Payroll payment) {
-
+        log.info("PAYROLL_SERVICE ::: Get request on " + env.getProperty("local.server.port") + " port");
         return ResponseEntity.ok().body(service.getPayroll(workerId, payment));
     }
 }
